a03dbf59a93451f0b7b7514f5cea091a58658e71
Mac Malone <mac@lean-fro.org>
2026-07-01 23:01:13 +0000

    chore:  `module` in ext/exe tests

--- tests/testExe.lean
+++ tests/testExe.lean
@@ -3,11 +3,12 @@ Copyright (c) 2026 Lean FRO. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mac Malone
 -/
+module
 import Nerodia
 
 open Nerodia
 
-def main : IO Unit := do
+public def main : IO Unit := do
   let pyVer ← PyIO.toIO do
     let sys ← Nerodia.import "sys"
     let ver ← sys.getAttrByString "version"
--- tests/testModule/lpl.lean
+++ tests/testModule/lpl.lean
@@ -3,6 +3,7 @@ Copyright (c) 2026 Lean FRO. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mac Malone
 -/
+module
 import Nerodia
 
 open Nerodia
@@ -12,7 +13,7 @@ Tests importing, from Lean, a Python module which uses Lean.
 That is, Lean-to-Python-to-Lean (LPL) integration.
 -/
 
-def main : IO Unit := do
+public def main : IO Unit := do
   let greeting ← PyIO.toIO do
     let mod ← Nerodia.import "testmodule"
     (← mod.getAttrByString "greeting").str
