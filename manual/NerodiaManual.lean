53f25a208f261e4db22ab37c202425c6c78be367
Mac Malone <mac@lean-fro.org>
2026-08-30 04:37:23 +0000

    doc: manual w/ README example

---
+++ manual/NerodiaManual.lean
@@ -0,0 +1,34 @@
+/-
+Copyright (c) 2026 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Mac Malone
+-/
+import VersoManual
+import NerodiaManual.Requirements
+import NerodiaManual.Tutorials
+
+open Verso.Genre Manual
+
+#doc (Manual) "Nerodia Reference Manual" =>
+%%%
+authors := ["Mac Malone"]
+shortTitle := "Nerodia Reference Manual"
+%%%
+
+Nerodia is a library for Lean/Python FFI inspired by [PyO3][1].
+It utilizes Lake, Lean's build system, to provide seamless integration.
+The name of the library comes from the genus _Nerodia_, a type of water snake.
+That is, a snake you would find in a lake.
+
+This manual provides tutorials to help new users get started, a general
+outline of Nerodia's structure, and a detailed reference of the Python API
+it exposes.
+
+*Important:* Nerodia (and this reference mnaual) is still a *work-in-progress*
+and currently has a very  limited API. It is released to the public primarily
+as a proof-of-concept and to obtain feedback on its design and build process.
+
+[1]: https://github.com/PyO3/pyo3
+
+{include 0 NerodiaManual.Requirements}
+{include 0 NerodiaManual.Tutorials}
---
+++ manual/NerodiaManual/Requirements.lean
@@ -0,0 +1,34 @@
+/-
+Copyright (c) 2026 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Mac Malone
+-/
+import VersoManual
+
+open Verso.Genre Manual
+
+#doc (Manual) "Requirements" =>
+%%%
+tag := "requirements"
+%%%
+
+In order to build a Nerodia project, all of the following are required:
+
+* Lean 4.33 or greater
+* CPython 3.14 or greater (shared; not free-threaded)
+* A C compiler which supports both (e.g., recent GCC or Clang; MSYS2's CLANG64 toolchain)
+
+A standard CPython distribution should usually be sufficient.
+[`uv`][1] and the [python.org installers][2] provide the necessary components.
+Some system package managers may also require a separate development package
+(e.g., `python3-dev`).
+
+Nonetheless, it is possible to write a Nerodia project without Python or a C
+compiler. These elements are only required to build and link the Python extension
+(or to use  `precompileModules`) and thus could be delegated to a CI (e.g.,
+GitHub Actions) that  can vendor these properly. Similarly, the Python extension
+can be distributed as a prebuilt binary (i.e., wheel), so users of the extension
+do not need Lean or a C compiler.
+
+[1]: https://github.com/astral-sh/uv
+[2]: https://www.python.org/downloads/
