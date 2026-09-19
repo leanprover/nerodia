0bcb155d54f8fe6166129b1ee8b25d84426c187d
Mac Malone <mac@lean-fro.org>
2026-09-19 20:55:30 +0000

    doc: update changelog

--- manual/NerodiaManual/Changelog.lean
+++ manual/NerodiaManual/Changelog.lean
@@ -3,13 +3,17 @@ Copyright (c) 2026 Lean FRO LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Mac Malone
 -/
-import VersoManual
 
-open Verso.Genre Manual
+import VersoManual
+import Nerodia
+
+open Nerodia
+open Verso.Genre Manual InlineLean
 
 #doc (Manual) "Changelog" =>
 %%%
 tag := "changelog"
+htmlSplit := .never
 %%%
 
 Highlights and breaking API changes for recent Nerodia versions appear here.
@@ -20,11 +24,11 @@ Lean version (i.e., Nerodia 4.X.0 for Lean 4.X.Y-Z), usually once per month.
 [1]: https://semver.org/
 
 Nerodia publishes a Git tag for each Lean stable using a `v` prefix (e.g.,
-`v4.33.X` for Lean `v4.33.X-Y`). If Nerodia needs to hot fix a bug, it will be
-listed hera as a new patch version (e.g., Nerodia 4.33.1), and the Git tags
+`v4.33.X` for Lean `v4.33.X-Y`). If Nerodia needs to update a release, it will
+be listed here as a new patch version (e.g., Nerodia 4.33.1), and the Git tags
 corresponding to the latest Lean stable release will be updated to point to it.
 For example, if Nerodia 4.33.1 was released and the latest Lean stable is Lean
-4.33.2, the `v4.33.2` tag will be updated but the `v4.33.1` tag will not.
+4.33.1, the `v4.33.1` tag will be updated but the `v4.33.0` tag will not.
 
 # Nerodia 4.34.0 (in development)
 %%%
@@ -33,11 +37,15 @@ tag := "4.34.0"
 
 - Introduction of this reference manual.
 
-- API for calling 0-arity and 1-arity Python functions
-(see, `PyObject.call0`, `PyObject.call1`).
+- Added {name}`Option` {ref "type-conversions"}`type conversions`.
 
-- Python integer support for fixed width Lean integer types
-(e.g., `IntX`, `UIntX`).
+- Added {lean}`PyBool` and {lean}`Bool` {ref "type-conversions"}`type conversions`.
+
+- API for calling 0-arity and 1-arity Python functions
+({lean}`PyObject.call0`, {lean}`PyObject.call1`).
+
+- Python integer support for fixed-width Lean integer types
+({tech}`IntX`, {tech}`UIntX`).
 
 # Nerodia 4.33.0
 %%%
